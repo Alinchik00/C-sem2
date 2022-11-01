1ba8c480ccf9bc6bc7da2999ca3efa35ebd6653d
Alina <kan.alina2017@yandex.ru>
2022-11-01 23:53:28 +0300

    задача на кратность чисел

---
+++ task5/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+int numA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число");
+int numB = int.Parse(Console.ReadLine()!);
+
+void res(int numA, int numB)
+{
+    int result = numA % numB;
+    if (result == 0)
+    {
+        Console.WriteLine("Первое число кратно второму");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не кратно второму");
+        Console.WriteLine("Остаток составляет: " + result);
+    }
+}
+
+res(numA, numB);
