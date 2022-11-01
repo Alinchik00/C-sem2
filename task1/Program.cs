1845e842b194be3015f87e428fc56e94100df0b8
Alina <kan.alina2017@yandex.ru>
2022-11-01 21:05:49 +0300

    решила задачу 1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int randomNumber = new Random().Next(10, 100);
+int firstDigit = randomNumber / 10;
+int secondDigit = randomNumber % 10;
+Console.WriteLine(randomNumber);
+Console.WriteLine(firstDigit);
+Console.WriteLine(secondDigit);
+if(firstDigit > secondDigit) 
+{
+    Console.WriteLine(firstDigit);
+}
+else
+Console.WriteLine(secondDigit);
