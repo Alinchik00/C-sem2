958fe21c3c870b76fdaaa277d2d5b9f1c670a9aa
Alina <kan.alina2017@yandex.ru>
2022-11-01 21:23:14 +0300

    задание 1, решение функцией

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int GenerateRandomNumber()
+{
+    return new Random().Next(10, 100);
+}
+
+int GetMaxDigit(int randomNumber)
+{
+    int firstDigit = randomNumber / 10;
+    int secondDigit = randomNumber % 10;
+
+    int max = firstDigit;
+
+    if(secondDigit > firstDigit)
+    max = secondDigit;
+    return max;
+}
+int randomNumber = GenerateRandomNumber();
+int max = GetMaxDigit(randomNumber);
+Console.WriteLine("Наибольшая цифра числа: " + max);
