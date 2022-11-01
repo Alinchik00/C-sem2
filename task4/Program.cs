7c8842a0a692284ac2f4efc45bfc80e61b790764
Alina <kan.alina2017@yandex.ru>
2022-11-01 21:59:23 +0300

    задача 2 через функцию

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int GenerateRandomNumber()
+{
+    return new Random().Next(100, 1000);
+}
+
+int OutSecondDigit(int randomNumber)
+{
+    Console.WriteLine(randomNumber);
+    int firstDigit = randomNumber / 100;
+    int secondDigit = (randomNumber - firstDigit*100) / 10;
+    int thirdDigit = randomNumber % 10;
+    return (firstDigit + thirdDigit);
+}
+
+int randomNumber = GenerateRandomNumber();
+
+Console.WriteLine(OutSecondDigit(randomNumber));
