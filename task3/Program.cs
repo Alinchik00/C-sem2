ed371c36d3aece371ca3e274fb1c78ba50fd04c2
Alina <kan.alina2017@yandex.ru>
2022-11-01 21:52:53 +0300

    задача 2, трехзначное число в двузначное

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine(randomNumber);
+int firstDigit = randomNumber / 100;
+int secondDigit = randomNumber % 100 / 10;
+int thirdDigit = randomNumber % 100 % 10;
+Console.WriteLine(firstDigit);
+Console.WriteLine(secondDigit);
+Console.WriteLine(thirdDigit);
+int newNumber = firstDigit*10 + thirdDigit;
+
+Console.WriteLine("Новое число: " + newNumber);
