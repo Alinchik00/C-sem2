d92e5780e412dc80f4e8a54d20728966f0dff0eb
Alina <kan.alina2017@yandex.ru>
2022-11-07 23:07:22 +0300

    задача на одновременную кратность

---
+++ task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+if (multiplicity(number))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+bool multiplicity(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
