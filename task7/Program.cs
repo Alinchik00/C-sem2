77711752ac2dfb58099527cbbedf387c365253aa
Alina <kan.alina2017@yandex.ru>
2022-11-08 00:26:24 +0300

    задача кратность в обе стороны

---
+++ task7/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int numA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число");
+int numB = int.Parse(Console.ReadLine()!);
+
+if (numA == numB * numB || numB == numA * numA)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
